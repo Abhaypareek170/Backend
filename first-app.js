920202a0ee33b0f14559f90e8f872495f2170797
Abhay Pareek <abhaypareek170@gmail.com>
2023-01-21 17:57:36 +0530

    Custom Urls

--- first-app.js
+++ first-app.js
@@ -1,7 +1,34 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log('Abhay');
+    const url = req.url;
+    if(url==='/home'){
+    res.write('<html>');
+    res.write('<head><title>First Project</title></head>');
+    res.write('<body><h1>Welcome to Home.</h1></body>')
+    res.write('</html>');
+    return res.end();
+    }
+    else if(url==='/about'){
+    res.write('<html>');
+    res.write('<head><title>First Project</title></head>');
+    res.write('<body><h1>Welcome to About.</h1></body>')
+    res.write('</html>');
+    return res.end();
+    }
+    else if(url==='/node'){
+        res.write('<html>');
+        res.write('<head><title>First Project</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project.</h1></body>')
+        res.write('</html>');
+        return res.end();
+        }
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>First Project</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(4000)
